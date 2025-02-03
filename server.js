8fa915f3393243655b86e248d589a2552cdcc402
waready <elbarto119@hotmail.com>
2025-02-02 23:37:47 -0500

    noce

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 const path = require('path');
 const ffmpegPath = require('ffmpeg-static');
+const { exec } = require('youtube-dl-exec');
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -42,39 +43,234 @@ app.get('/download', async (req, res) => {
         const artist = info.videoDetails.author.name;
         const videoId = info.videoDetails.videoId;
         const audioPath = `public/${videoId}.mp3`;
-
+        console.log(info)
         // Descargar y convertir el video a MP3
         const audioStream = ytdl(url, { filter: 'audioonly' });
 
-        ffmpeg(audioStream)
-            .audioBitrate(128)
-            .save(audioPath)
-            .on('end', () => {
-                // Verificar si la canción ya está en la lista de reproducción
-                const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
-                if (existingSongIndex === -1) {
-                    // Agregar la canción a la lista de reproducción si no está duplicada
-                    playlist.push({ title, artist, audioPath: `${videoId}.mp3` });
-                }
-                
-                if (currentSongIndex === -1) {
-                    // Si no hay ninguna canción en reproducción, reproducir la nueva canción
-                    currentSongIndex = 0;
-                    playCurrentSong();
-                }
+        exec(url, {
+            extractAudio: true,
+            audioFormat: 'mp3', // Cambiado a 'mp3'
+            output: audioPath,
+            ffmpegLocation: ffmpegPath,
+        })
+        .then(outputPath => {
+            // Verificar si la canción ya está en la lista de reproducción
+            const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
+            if (existingSongIndex === -1) {
+                // Agregar la canción a la lista de reproducción si no está duplicada
+                playlist.push({ title, artist, audioPath: `${videoId}.mp3` });
+            }
+            
+            if (currentSongIndex === -1) {
+                // Si no hay ninguna canción en reproducción, reproducir la nueva canción
+                currentSongIndex = 0;
+                playCurrentSong();
+            }
 
-                // Enviar la respuesta como JSON
-                res.json({ title, artist, audio_path: `${videoId}.mp3` });
-            })
-            .on('error', (err) => {
-                console.error(err);
-                res.status(500).json({ error: 'Failed to convert video to MP3' });
-            });
+            // Enviar la respuesta como JSON
+            res.json({ title, artist, audio_path: `${videoId}.mp3` });
+        })
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to process the YouTube URL' });
     }
 });
+
+
+
+// app.get('/download', async (req, res) => {
+//     const url = req.query.url;
+//     if (!url) {
+//         return res.status(400).json({ error: 'URL parameter is required' });
+//     }
+
+//     try {
+//         // Obtener información del video de YouTube
+//         const info = await ytdl.getInfo(url);
+//         const title = info.videoDetails.title;
+//         const artist = info.videoDetails.author.name;
+//         const videoId = info.videoDetails.videoId;
+//         const videoPath = `public/${videoId}.mp4`;
+
+//         // Descargar el video en formato MP4
+//         exec(url, {
+//             format: 'mp4', // Cambiado a 'mp4' para descargar video
+//             output: videoPath,
+//             ffmpegLocation: ffmpegPath,
+//         })
+//         .then(outputPath => {
+//             // Verificar si la canción ya está en la lista de reproducción
+//             const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
+//             if (existingSongIndex === -1) {
+//                 // Agregar la canción a la lista de reproducción si no está duplicada
+//                 playlist.push({ title, artist, audioPath: `${videoId}.mp4` });
+//             }
+
+//             if (currentSongIndex === -1) {
+//                 // Si no hay ninguna canción en reproducción, reproducir la nueva canción
+//                 currentSongIndex = 0;
+//                 playCurrentSong();
+//             }
+
+//             // Enviar la respuesta como JSON
+//             res.json({ title, artist, video_path: `${videoId}.mp4` });
+//         })
+//         .catch(error => {
+//             console.error('Error al descargar el video:', error);
+//             res.status(500).json({ error: 'Failed to download the YouTube video' });
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Failed to process the YouTube URL' });
+//     }
+// });
+
+
+// app.get('/download', async (req, res) => {
+//     const url = req.query.url;
+//     if (!url) {
+//         return res.status(400).json({ error: 'URL parameter is required' });
+//     }
+
+//     try {
+//         // Obtener información del video de YouTube
+//         const info = await ytdl.getInfo(url);
+//         const title = info.videoDetails.title;
+//         const artist = info.videoDetails.author.name;
+//         const videoId = info.videoDetails.videoId;
+//         const videoPath = `public/${videoId}.mp4`;
+
+//         // Descargar el video en la mejor calidad disponible
+//         exec(url, {
+//             format: 'bestvideo+bestaudio/best', // Descargar la mejor calidad de video y audio
+//             output: videoPath,
+//             ffmpegLocation: ffmpegPath,
+//         })
+//         .then(outputPath => {
+//             // Verificar si la canción ya está en la lista de reproducción
+//             const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
+//             if (existingSongIndex === -1) {
+//                 // Agregar la canción a la lista de reproducción si no está duplicada
+//                 playlist.push({ title, artist, videoPath: `${videoId}.mp4` });
+//             }
+
+//             if (currentSongIndex === -1) {
+//                 // Si no hay ninguna canción en reproducción, reproducir la nueva canción
+//                 currentSongIndex = 0;
+//                 playCurrentSong();
+//             }
+
+//             // Enviar la respuesta como JSON
+//             res.json({ title, artist, video_path: `${videoId}.mp4` });
+//         })
+//         .catch(error => {
+//             console.error('Error al descargar el video:', error);
+//             res.status(500).json({ error: 'Failed to download the YouTube video' });
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Failed to process the YouTube URL' });
+//     }
+// });
+
+
+// app.get('/download', async (req, res) => {
+//     const url = req.query.url;
+//     if (!url) {
+//         return res.status(400).json({ error: 'URL parameter is required' });
+//     }
+
+//     try {
+//         // Obtener información del video de YouTube
+//         const info = await ytdl.getInfo(url);
+//         const title = info.videoDetails.title;
+//         const artist = info.videoDetails.author.name;
+//         const videoId = info.videoDetails.videoId;
+//         const videoPath = `public/${videoId}.mp4`;
+
+//         // Descargar el video en la mejor calidad disponible y forzar la salida en MP4
+//         exec(url, {
+//             format: 'bestvideo+bestaudio/best', // Descargar la mejor calidad de video y audio
+//             output: videoPath,
+//             ffmpegLocation: ffmpegPath,
+//             mergeOutputFormat: 'mp4', // Forzar la salida en MP4
+//         })
+//         .then(outputPath => {
+//             // Verificar si la canción ya está en la lista de reproducción
+//             const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
+//             if (existingSongIndex === -1) {
+//                 // Agregar la canción a la lista de reproducción si no está duplicada
+//                 playlist.push({ title, artist, videoPath: `${videoId}.mp4` });
+//             }
+
+//             if (currentSongIndex === -1) {
+//                 // Si no hay ninguna canción en reproducción, reproducir la nueva canción
+//                 currentSongIndex = 0;
+//                 playCurrentSong();
+//             }
+
+//             // Enviar la respuesta como JSON
+//             res.json({ title, artist, video_path: `${videoId}.mp4` });
+//         })
+//         .catch(error => {
+//             console.error('Error al descargar el video:', error);
+//             res.status(500).json({ error: 'Failed to download the YouTube video' });
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Failed to process the YouTube URL' });
+//     }
+// });
+
+
+// app.get('/download', async (req, res) => {
+//     const url = req.query.url;
+//     if (!url) {
+//         return res.status(400).json({ error: 'URL parameter is required' });
+//     }
+
+//     try {
+//         // Obtener información del video de YouTube
+//         const info = await ytdl.getInfo(url);
+//         const title = info.videoDetails.title;
+//         const artist = info.videoDetails.author.name;
+//         const videoId = info.videoDetails.videoId;
+//         const videoPath = `public/${videoId}.mp4`;
+
+//         // Descargar el video en la mejor calidad disponible y forzar la salida en MP4
+//         exec(url, {
+//             format: 'bestvideo+bestaudio/best', // Descargar la mejor calidad de video y audio
+//             output: videoPath,
+//             ffmpegLocation: ffmpegPath,
+//             mergeOutputFormat: 'mp4', // Forzar la salida en MP4
+//             //postprocessorArgs: ['-c:v', 'copy', '-c:a', 'libmp3lame'], // Convertir el audio a AAC para compatibilidad con MP4
+//         })
+//         .then(outputPath => {
+//             // Verificar si la canción ya está en la lista de reproducción
+//             const existingSongIndex = playlist.findIndex(song => song.title === title && song.artist === artist);
+//             if (existingSongIndex === -1) {
+//                 // Agregar la canción a la lista de reproducción si no está duplicada
+//                 playlist.push({ title, artist, videoPath: `${videoId}.mp4` });
+//             }
+
+//             if (currentSongIndex === -1) {
+//                 // Si no hay ninguna canción en reproducción, reproducir la nueva canción
+//                 currentSongIndex = 0;
+//                 playCurrentSong();
+//             }
+
+//             // Enviar la respuesta como JSON
+//             res.json({ title, artist, video_path: `${videoId}.mp4` });
+//         })
+//         .catch(error => {
+//             console.error('Error al descargar el video:', error);
+//             res.status(500).json({ error: 'Failed to download the YouTube video' });
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Failed to process the YouTube URL' });
+//     }
+// });
 
 // Servir archivos estáticos
 app.use(express.static('public'));
